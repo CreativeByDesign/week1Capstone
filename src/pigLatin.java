619f695da0fd8b6055ec5587cc94ab4e857629d6
DeVosha <devoshapatterson@DeVoshas-MBP.router>
2019-04-29 04:47:09 -0400

    pig latin with errors

---
+++ src/pigLatin.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pigLatin {
+
+	public static void main(String[] args) {
+		String word;
+		Scanner scan = new Scanner(System.in);
+
+		word = scan.nextLine().toLowerCase();
+
+		System.out.println(word);
+		scan.close();
+	} 
+
+
+	// public static String pigLatin(String word) {
+
+}
